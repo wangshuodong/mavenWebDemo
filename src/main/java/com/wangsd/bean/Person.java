b93cb3c3cd1b9c817ba5e119cb02b1f2b5ad80cc
wangshuodong <wangshuodong@chinasoftinc.com>
2016-12-15 11:24:25 +0800

    Initial commit

---
+++ src/main/java/com/wangsd/bean/Person.java
@@ -0,0 +1,25 @@
+package com.wangsd.bean;
+
+/**
+ * Created by Administrator on 2016/12/15.
+ */
+public class Person {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
