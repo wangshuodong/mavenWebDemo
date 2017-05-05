3210d87a86aae2158c99a11c7e7da572e4662f89
wangshuodong <wangshuodong2004@163.com>
2017-05-05 17:38:09 +0800

    使用log4j2记录日志

--- src/main/java/com/wangsd/controller/SysController.java
+++ src/main/java/com/wangsd/controller/SysController.java
@@ -5,19 +5,29 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2017/1/13.
  */
 @Controller
+@RequestMapping("/hello")
 public class SysController {
 
     private static final Logger logger = LogManager.getLogger(SysController.class.getName());
 
-    @RequestMapping("/hello")
-    public String hello(Model model) {
 
-            int a = 2 / 0;
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello1(Model model) {
+
+        model.addAttribute("greeting", "Hello Spring MVC");
+        return "helloworld";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello2(Model model) {
 
         model.addAttribute("greeting", "Hello Spring MVC");
         return "helloworld";
